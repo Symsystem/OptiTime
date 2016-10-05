b280559e3d88e315234bf2fc10ccd69040c8d53e
Symsystem <symeon.delmarmol@gmail.com>
2016-10-05 15:30:56 +0200

    use first command

--- app/src/main/java/com/symsystem/optitime/application/task/TaskApplicationService.java
+++ app/src/main/java/com/symsystem/optitime/application/task/TaskApplicationService.java
@@ -1,10 +1,28 @@
 package com.symsystem.optitime.application.task;
 
+import com.symsystem.optitime.application.task.Command.CreateTask;
+import com.symsystem.optitime.domain.task.Task;
+import com.symsystem.optitime.domain.task.TaskId;
+import com.symsystem.optitime.repository.TaskRepository;
+
 /**
  * @author sym
  */
 
 public class TaskApplicationService {
 
+    private final TaskRepository taskRepository;
 
+    public TaskApplicationService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public String createTask(CreateTask command) {
+
+        TaskId id = new TaskId(command.getTaskId());
+
+        Task task = new Task(id, command.getName());
+
+        return id.id();
+    }
 }
